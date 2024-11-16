becc8b9fe04617071d771df033488f2a1a0d0bf4
PrachiJunwaleCodingal <prachi.junwale@gmail.com>
2024-11-16 22:07:09 +0530

    hi

--- L1/Act1.py
+++ L1/Act1.py
@@ -1,1 +1,4 @@
-print("Hello Prachi")
+print("Hello")
+print("My name is Prachi")
+print("I like Coding")
+print("Addition of 10+5 is ", 10+5)
---
+++ L1/demo.py
@@ -0,0 +1,3 @@
+print("Hello, My name is Prachi")
+print("I like to code")
+print("Answer of 10+5*8 is", 10+5*8)
